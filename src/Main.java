4bca0a82b389b74fcdbeb305376ab87d5683fbba
NnazZzaR <tuchapetsnazar.ua@gmail.com>
2022-04-04 01:17:38 +0300

    Task#1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args){
-
+        // homework 04.04
+        checkBrick first = new checkBrick();
+        first.check(3, 2, 1, 1, 2);
     }
 }
---
+++ src/checkBrick.java
@@ -0,0 +1,18 @@
+public class checkBrick {
+    public void check(int a, int b, int c, int x, int y){
+        System.out.print("Task#1 : ");
+        // a - width, b - length, c - height of brick
+        // x - width, y - length of rectangle
+        if (a <= 0 || b <= 0 || c <= 0 || x <= 0 || y <= 0){
+            System.out.println("Variables must be > 0");
+        }
+        if (((x - a) >= 0 && (y - b) >= 0) || ((x - b) >= 0 && (y - a) >= 0 ) ||
+        ((x - a) >= 0 && (y - c) >= 0) || ((x - c) >= 0 && (y - a) >= 0) ||
+                ((x - c) >= 0 && (y - b) >= 0) || ((x - b) >= 0 && (y - c) >= 0)){
+            System.out.println("Congratulation :)");
+        }
+        else {
+            System.out.println("Error :(");
+        }
+    }
+}
